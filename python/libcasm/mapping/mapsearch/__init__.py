153dcc275679ab33a25ad98f25a6d3ea2f024e86
bpuchala <bpuchala@users.noreply.github.com>
2023-08-10 22:21:33 -0400

    fix pybind11_add_module targets and use explicit imports

--- python/libcasm/mapping/mapsearch/__init__.py
+++ python/libcasm/mapping/mapsearch/__init__.py
@@ -1,1 +1,16 @@
 """Construct custom mapping search algorithms"""
+from ._mapsearch import (
+    AtomMappingSearchData,
+    IsotropicAtomCost,
+    LatticeMappingSearchData,
+    MappingNode,
+    MappingSearch,
+    PrimSearchData,
+    QueueConstraints,
+    StructureSearchData,
+    SymmetryBreakingAtomCost,
+    WeightedTotalCost,
+    make_atom_to_site_cost,
+    make_superstructure_data,
+    make_trial_translations,
+)
--- python/libcasm/mapping/methods/__init__.py
+++ python/libcasm/mapping/methods/__init__.py
@@ -1,1 +1,8 @@
 """Easy-to-use mapping methods"""
+from ._methods import (
+    make_mapped_lattice,
+    make_mapped_structure,
+    map_atoms,
+    map_lattices,
+    map_structures,
+)
